90d2f3bbf932b4f4e1f1972d97e0cb4875e575f1
amcdevitt97 <amcdevitt97@gmail.com>
2018-10-29 19:57:57 -0400

    finished pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,60 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card] 
+    (get {\2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14} rank)
+  )
+)
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card] (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [count (apply max (vals (frequencies (map rank hand)))) ]
+    (> count 1)
+    ))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [count (apply max (vals (frequencies (map rank hand)))) ]
+    (== count 3)
+    ))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [count (apply max (vals (frequencies (map rank hand)))) ]
+    (== count 4)
+    ))
 
 (defn flush? [hand]
-  nil)
+  (== (apply max (vals (frequencies (map suit hand))))5))
 
 (defn full-house? [hand]
-  nil)
+  (= (seq [2 3]) (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+   (= 2 (get (frequencies (vals (frequencies (map rank hand))))2))
+   (= 1 (get (frequencies (vals (frequencies (map rank hand))))4)) 
+  )
+)
 
 (defn straight? [hand]
-  nil)
+  (let [ sorted (sort (map rank hand)) smallest (first sorted)
+        sorted2 (sort (replace {14 1} sorted)) secondSmallest (first sorted2) ]
+    (or
+      (= sorted (range smallest (+ smallest 5)))
+      (= sorted2 (range secondSmallest (+ secondSmallest 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+ (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
